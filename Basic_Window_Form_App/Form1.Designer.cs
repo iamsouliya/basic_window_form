c6d10063916f55610fe04e722d0f082196dd01b8
iamsouliya <souliyatmv24@gmail.com>
2023-11-08 19:07:13 +0700

    feature calculator

--- Basic_Window_Form_App/Form1.Designer.cs
+++ Basic_Window_Form_App/Form1.Designer.cs
@@ -78,6 +78,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
             // button4
             // 
@@ -88,6 +89,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "+";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -98,6 +100,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "4";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -108,6 +111,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "5";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -118,6 +122,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "6";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -139,6 +144,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "7";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -149,6 +155,7 @@
             this.button10.TabIndex = 9;
             this.button10.Text = "8";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -159,6 +166,7 @@
             this.button11.TabIndex = 10;
             this.button11.Text = "9";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -169,6 +177,7 @@
             this.button12.TabIndex = 11;
             this.button12.Text = "x";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
@@ -179,6 +188,7 @@
             this.button13.TabIndex = 12;
             this.button13.Text = "0";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
@@ -189,6 +199,7 @@
             this.button14.TabIndex = 13;
             this.button14.Text = "=";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
@@ -199,6 +210,7 @@
             this.button15.TabIndex = 14;
             this.button15.Text = "C";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
@@ -209,6 +221,7 @@
             this.button16.TabIndex = 15;
             this.button16.Text = "/";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // label1
             // 
--- Basic_Window_Form_App/Form1.cs
+++ Basic_Window_Form_App/Form1.cs
@@ -6,7 +6,10 @@ namespace Basic_Window_Form_App
 {
     public partial class Form1 : Form
     {
-
+        int input1 = 0;
+        int input2 = 0;
+        int result = 0;
+        char op;
         public Form1()
         {
             InitializeComponent();
@@ -27,13 +30,7 @@ namespace Basic_Window_Form_App
 
         }
 
-
         private void timer1_Tick(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button8_Click(object sender, EventArgs e)
         {
 
         }
@@ -50,12 +47,113 @@ namespace Basic_Window_Form_App
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            label1.Text = "1";
+            label1.Text += "1";
         }
 
         private void button2_Click_1(object sender, EventArgs e)
         {
+            label1.Text += "2";
+        }
 
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            label1.Text += "3";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text += "4";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text += "6";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text += "7";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text += "8";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text += "9";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            label1.Text += "0";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            result = 0;
+            input1 = 0;
+            input2 = 0;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            input2 = int.Parse(label1.Text);
+            if (op == '+')
+            {
+                result = input1 + input2;
+                label1.Text = result.ToString();
+            }
+            else if (op == '-')
+            {
+                result = input1 - input2;
+                label1.Text = result.ToString();
+            }
+            else if (op == '*')
+            {
+                result = input1 * input2;
+                label1.Text = result.ToString();
+            }
+            else if (op == '/')
+            {
+                result = input1 / input2;
+                label1.Text = result.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            input1 = int.Parse(label1.Text);
+            label1.Text = "";
+            op = '+';
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            input1 = int.Parse(label1.Text);
+            label1.Text = "";
+            op = '-';
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            input1 = int.Parse(label1.Text);
+            label1.Text = "";
+            op = '*';
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            input1 = int.Parse(label1.Text);
+            label1.Text = "";
+            op = '/';
         }
     }
 }
